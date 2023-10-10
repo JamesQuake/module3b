b16aed24984a3f7ec9bf78c6fe4b7c4cec8a6ac7
James Quake <jcottrelliii@gmail.com>
2023-10-10 13:55:28 -0500

    python 3b

---
+++ main3b.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+#“Hello World!” is an argument pass to the print() function
+print(1 + 2)
+print(7 * 6)
+print()
+print("The end")
+Print("The end", "or is it?", "stay tuned to learn more about Python", 3)
